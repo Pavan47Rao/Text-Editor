7677d4a693db5c07e2d17a3450da65f1d0ae3daa
Pavan Rao <srkantarao.p@husky.neu.edu>
2019-10-20 22:01:24 -0400

    Corrected logic of overlapping styles, closed event listeners on window close, added header to the page

--- editor.js
+++ editor.js
@@ -2,35 +2,35 @@ let boldBtn = document.getElementById('bold-btn');
 let boldClickListener = (event) => {
     event.preventDefault();
     let selection = window.getSelection();
-    let final = `<span class="text-bold">${selection.toString()}</span>`;
-    // let abc= `${selection.getRangeAt(0).startContainer}</span>`;
-    // document.getSelection().anchorNode.parentElement.innerHTML = document.getSelection().anchorNode.parentElement.innerHTML.replace("<span","</span><span");
-    if(selection.anchorNode.parentElement.classList.contains("text-bold"))
-    selection.anchorNode.parentElement.classList.remove("text-bold");
-    else
-    document.getSelection().anchorNode.parentElement.innerHTML = document.getSelection().anchorNode.parentElement.innerHTML.replace(document.getSelection().toString(),final);
+    let range = window.getSelection().getRangeAt(0);
+    if(!selection.anchorNode.parentElement.classList.contains("text-bold")) {
+        let selectionContents = range.extractContents();
+        let span = document.createElement("span");
+        span.className = "text-bold";
+        span.appendChild(selectionContents);
+        range.insertNode(span);
+    }
+    else {
+        selection.anchorNode.parentElement.classList.remove("text-bold");
+    }
 };
-
-// let boldClickListener = (event) => {
-//     event.preventDefault();
-//     let selection = document.getSelection();
-//     if(selection.focusNode.parentElement.classList.contains("text-italic")) {
-//         let abc = `<span class="text-bold">${selection.getRangeAt(0).startContainer.data}</span><span class="text-bold text-italic">${selection.getRangeAt(0).endContainer.data}</span>`;
-//         selection.anchorNode.parentElement.innerHTML = abc;
-//     }
-// };
-
 boldBtn.addEventListener('click',boldClickListener);
 
 let italicBtn = document.getElementById('italic-btn');
 let italicClickListener = (event) => {
     event.preventDefault();
     let selection = window.getSelection();
-    let final = `<span class="text-italic">${selection.toString()}</span>`;
-    if(selection.anchorNode.parentElement.classList.contains("text-italic"))
-    selection.anchorNode.parentElement.classList.remove("text-italic");
-    else
-    document.getSelection().anchorNode.parentElement.innerHTML = document.getSelection().anchorNode.parentElement.innerHTML.replace(document.getSelection().toString(),final);
+    let range = window.getSelection().getRangeAt(0);
+    if(!selection.anchorNode.parentElement.classList.contains("text-italic")) {
+        let selectionContents = range.extractContents();
+        let span = document.createElement("span");
+        span.className = "text-italic";
+        span.appendChild(selectionContents);
+        range.insertNode(span);
+    }
+    else {
+        selection.anchorNode.parentElement.classList.remove("text-italic");
+    }
 };
 italicBtn.addEventListener('click',italicClickListener);
 
@@ -38,11 +38,17 @@ let underlineBtn = document.getElementById('underline-btn');
 let underlineClickListener = (event) => {
     event.preventDefault();
     let selection = window.getSelection();
-    let final = `<span class="text-underline">${selection.toString()}</span>`;
-    if(selection.anchorNode.parentElement.classList.contains("text-underline"))
-    selection.anchorNode.parentElement.classList.remove("text-underline");
-    else
-    document.getSelection().anchorNode.parentElement.innerHTML = document.getSelection().anchorNode.parentElement.innerHTML.replace(document.getSelection().toString(),final);
+    let range = window.getSelection().getRangeAt(0);
+    if(!selection.anchorNode.parentElement.classList.contains("text-italic")) {
+        let selectionContents = range.extractContents();
+        let span = document.createElement("span");
+        span.className = "text-underline";
+        span.appendChild(selectionContents);
+        range.insertNode(span);
+    }
+    else {
+        selection.anchorNode.parentElement.classList.remove("text-italic");
+    }
 };
 underlineBtn.addEventListener('click',underlineClickListener);
 
@@ -50,15 +56,15 @@ let alignCenterBtn = document.getElementById('align-center-btn');
 let alignCenterClickListener = (event) => {
     event.preventDefault();
     let classList = document.getElementById('container').classList;
-    if(classList.contains("text-align-center") || classList.contains("text-align-left") || classList.contains("text-align-right")) {
-        let classes = Array.from(document.getElementById('container').classList);
-        let existingModifier = classes.filter(className => { return className.match(/^text-/) });
-        document.getElementById('container').classList.remove(existingModifier);
-        document.getElementById('container').classList.add("text-align-center");
-    }
-    else {
-        document.getElementById('container').classList.add("text-align-center");
-    }    
+        if(classList.contains("text-align-center") || classList.contains("text-align-left") || classList.contains("text-align-right")) {
+            let classes = Array.from(document.getElementById('container').classList);
+            let existingModifier = classes.filter(className => { return className.match(/^text-align-/) });
+            document.getElementById('container').classList.remove(existingModifier);
+            document.getElementById('container').classList.add("text-align-center");
+        }
+        else {
+            document.getElementById('container').classList.add("text-align-center");
+        }  
 };
 alignCenterBtn.addEventListener('click',alignCenterClickListener);
 
@@ -66,15 +72,15 @@ let alignLeftBtn = document.getElementById('align-left-btn');
 let alignLeftClickListener = (event) => {
     event.preventDefault();
     let classList = document.getElementById('container').classList;
-    if(classList.contains("text-align-center") || classList.contains("text-align-left") || classList.contains("text-align-right")) {
-        let classes = Array.from(document.getElementById('container').classList);
-        let existingModifier = classes.filter(className => { return className.match(/^text-/) });
-        document.getElementById('container').classList.remove(existingModifier);
-        document.getElementById('container').classList.add("text-align-left");
-    }
-    else {
-        document.getElementById('container').classList.add("text-align-left");
-    }    
+        if(classList.contains("text-align-center") || classList.contains("text-align-left") || classList.contains("text-align-right")) {
+            let classes = Array.from(document.getElementById('container').classList);
+            let existingModifier = classes.filter(className => { return className.match(/^text-align-/) });
+            document.getElementById('container').classList.remove(existingModifier);
+            document.getElementById('container').classList.add("text-align-left");
+        }
+        else {
+            document.getElementById('container').classList.add("text-align-left");
+        }  
 };
 alignLeftBtn.addEventListener('click',alignLeftClickListener);
 
@@ -82,14 +88,23 @@ let alignRightBtn = document.getElementById('align-right-btn');
 let alignRightClickListener = (event) => {
     event.preventDefault();
     let classList = document.getElementById('container').classList;
-    if(classList.contains("text-align-center") || classList.contains("text-align-left") || classList.contains("text-align-right")) {
-        let classes = Array.from(document.getElementById('container').classList);
-        let existingModifier = classes.filter(className => { return className.match(/^text-/) });
-        document.getElementById('container').classList.remove(existingModifier);
-        document.getElementById('container').classList.add("text-align-right");
-    }
-    else {
-        document.getElementById('container').classList.add("text-align-right");
-    }    
+        if(classList.contains("text-align-center") || classList.contains("text-align-left") || classList.contains("text-align-right")) {
+            let classes = Array.from(document.getElementById('container').classList);
+            let existingModifier = classes.filter(className => { return className.match(/^text-align-/) });
+            document.getElementById('container').classList.remove(existingModifier);
+            document.getElementById('container').classList.add("text-align-right");
+        }
+        else {
+            document.getElementById('container').classList.add("text-align-right");
+        }   
 };
 alignRightBtn.addEventListener('click',alignRightClickListener);
+
+window.onbeforeunload = function() {
+    boldBtn.removeEventListener("click", boldClickListener);
+    italicBtn.removeEventListener("click", italicClickListener);
+    underlineBtn.removeEventListener("click", underlineClickListener);
+    alignCenterBtn.removeEventListener("click", alignCenterClickListener);
+    alignLeftBtn.removeEventListener('click', alignLeftClickListener);
+    alignRightBtn.removeEventListener('click', alignRightClickListener);
+}
